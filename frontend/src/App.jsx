24063f329f530c66ef302e7a492fd1dcaa94c221
Mayank Kaushik <kaushmayank218@gmail.com>
2025-06-21 07:12:08 +0530

    making Zerodha components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import HomePage from './landing_page/home/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-       
-      </div>
+       <HomePage/>
     </>
   )
 }
